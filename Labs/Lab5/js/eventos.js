09c10be149417421ed6031e2b6ae7d9a59b10459
EugenioSa <A01701996@itesm.mx>
2018-08-23 01:38:32 -0500

    Lab 5 Estilo

---
+++ Labs/Lab5/js/eventos.js
@@ -0,0 +1,29 @@
+function password_validador(){
+    
+    
+    
+    
+    
+    
+}
+
+
+
+function compra_productos(){
+    
+    
+    
+       
+    
+    
+}
+
+
+
+function validador_personal(){
+    
+    
+    
+    
+    
+}
