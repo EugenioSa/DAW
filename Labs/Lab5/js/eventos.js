663ba6551fd4f144d6121077dc2270c9c89bac88
EugenioSa <A01701996@itesm.mx>
2018-08-23 23:55:20 -0500

    Funciones tienda

--- Labs/Lab5/js/eventos.js
+++ Labs/Lab5/js/eventos.js
@@ -25,23 +25,70 @@ function password_validator() {
 
 
 
+var acum_p=0,acum_s=0,acum_t = 0;
 
-
-function compra_productos(clicked_id){
+function pri_producto_men(){
+    acum_p = acum_p -1;
+   document.getElementById("a1").addEventListener("click", function(){
+         document.getElementById("p").innerHTML = acum_p;
+   });
     
-       
-    if((clicked_id === a1) || (clicked_id === a2)){
-        
-        alert("hola");
-        
-    }else 
-    if((clicked_id == "b1") || (clicked_id == "b2")){
-        
-    }else   
-    if((clicked_id == "c1") || (clicked_id == "c2")){
-        
-        
-    }
+}
+
+
+function pri_producto_mas(clicked_id){
+    
+    acum_p = acum_p +1;
+   document.getElementById("a2").addEventListener("click", function(){
+         document.getElementById("p").innerHTML = acum_p;
+   });
+    
+}
+
+function seg_producto_men(){
+    acum_s = acum_s -1;
+   document.getElementById("b1").addEventListener("click", function(){
+         document.getElementById("s").innerHTML = acum_s;
+   });
+    
+}
+
+
+function seg_producto_mas(clicked_id){
+    
+    acum_s = acum_s +1;
+   document.getElementById("b2").addEventListener("click", function(){
+         document.getElementById("s").innerHTML = acum_s;
+   });
+    
+}
+
+
+function ter_producto_men(){
+    acum_t = acum_t -1;
+   document.getElementById("c1").addEventListener("click", function(){
+         document.getElementById("t").innerHTML = acum_t;
+   });
+    
+}
+
+
+function ter_producto_mas(clicked_id){
+    
+    acum_t = acum_t +1;
+   document.getElementById("c2").addEventListener("click", function(){
+         document.getElementById("t").innerHTML = acum_t;
+   });
+    
+}
+
+var acum_total = 0;
+
+function total(){
+    
+   document.getElementById("tot").addEventListener("click", function(){
+         document.getElementById("total").innerHTML = acum_total;
+   });
     
 }
 
