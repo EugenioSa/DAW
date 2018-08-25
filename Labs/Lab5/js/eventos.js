1b58ba4595f667fdf29ed805c54e18323b7d9fce
EugenioSa <A01701996@itesm.mx>
2018-08-25 13:48:15 -0500

    Funcionalidad total tienda

--- Labs/Lab5/js/eventos.js
+++ Labs/Lab5/js/eventos.js
@@ -25,13 +25,20 @@ function password_validator() {
 
 
 
-var acum_p=0,acum_s=0,acum_t = 0;
+var acum_p=0,acum_s=0,acum_t = 0,acum_total=0;
 
 function pri_producto_men(){
-    acum_p = acum_p -1;
+    
+    if(acum_p>0 || ((acum_p-1)==0)){
+        acum_p = acum_p -1;
+    
+        acum_total= acum_total-3000;
+    }
    document.getElementById("a1").addEventListener("click", function(){
          document.getElementById("p").innerHTML = acum_p;
    });
+    
+    total(acum_total);
     
 }
 
@@ -39,36 +46,62 @@ function pri_producto_men(){
 function pri_producto_mas(clicked_id){
     
     acum_p = acum_p +1;
+    acum_total= acum_total+3000;
+    
+    
    document.getElementById("a2").addEventListener("click", function(){
          document.getElementById("p").innerHTML = acum_p;
    });
     
+    total(acum_total);
+    
 }
 
 function seg_producto_men(){
-    acum_s = acum_s -1;
+    
+    if(acum_s>0 || ((acum_s-1)==0)){
+        acum_s = acum_s -1;
+        acum_total= acum_total-2500;
+    }
+    
+    
    document.getElementById("b1").addEventListener("click", function(){
          document.getElementById("s").innerHTML = acum_s;
    });
     
+    total(acum_total);
 }
 
 
 function seg_producto_mas(clicked_id){
     
     acum_s = acum_s +1;
+    acum_total= acum_total+2500;
+    
+    
    document.getElementById("b2").addEventListener("click", function(){
          document.getElementById("s").innerHTML = acum_s;
    });
+    
+    total(acum_total);
     
 }
 
 
 function ter_producto_men(){
-    acum_t = acum_t -1;
+    
+    if(acum_t>0 || ((acum_t-1)==0)){
+        
+        acum_t = acum_t -1;
+        acum_total= acum_total-5000;
+    }
+    
+    total(acum_total);
    document.getElementById("c1").addEventListener("click", function(){
          document.getElementById("t").innerHTML = acum_t;
    });
+    
+    total(acum_total);
     
 }
 
@@ -76,19 +109,26 @@ function ter_producto_men(){
 function ter_producto_mas(clicked_id){
     
     acum_t = acum_t +1;
+    acum_total= acum_total+5000;
+    
+    total(acum_total);
    document.getElementById("c2").addEventListener("click", function(){
          document.getElementById("t").innerHTML = acum_t;
    });
     
+    total(acum_total);
+    
 }
 
-var acum_total = 0;
 
-function total(){
+
+function total(total_compra){
     
-   document.getElementById("tot").addEventListener("click", function(){
-         document.getElementById("total").innerHTML = acum_total;
-   });
+         document.getElementById("tot").innerHTML = acum_total;
+    
+        document.getElementById("totiva").innerHTML = acum_total + (acum_total*.03);
+    
+    
     
 }
 
